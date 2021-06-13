9a7398219a632b9fdf73973147e640232fa3e60a
hoangphuc97 <phuchc@d-soft.com.vn>
2021-06-13 20:32:53 +0700

    init terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,3 +3,13 @@ terraform {
     
   }
 }
+
+data "terraform_remote_state" "example" {
+  backend = "http"
+
+  config = {
+    address = var.address
+    username = var.username
+    password = var.password
+  }
+}
--- terraform/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "example" {
-  backend = "http"
-
-  config = {
-    address = var.address
-    username = var.username
-    password = var.password
-  }
-}
