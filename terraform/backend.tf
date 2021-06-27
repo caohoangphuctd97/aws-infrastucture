035940b6a67e8b43da89294bd095529a16c31889
hoangphuc97 <phuchc@d-soft.com.vn>
2021-06-27 16:16:42 +0700

    create s3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,13 +3,3 @@ terraform {
     
   }
 }
-
-data "terraform_remote_state" "example" {
-  backend = "http"
-
-  config = {
-    address = var.address
-    username = var.username
-    password = var.password
-  }
-}
