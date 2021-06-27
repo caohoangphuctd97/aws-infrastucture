a93b3e0fa22d4af3a6c5a9bc7f31c7868979175a
hoangphuc97 <phuchc@d-soft.com.vn>
2021-06-27 16:06:25 +0700

    create s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,12 @@ provider "aws" {
     region = var.region
 }
 
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
