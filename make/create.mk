1573927b5fc01465fddf1fa06b5ffc2106ff6d79
hoangphuc97 <phuchc@d-soft.com.vn>
2021-06-28 01:50:16 +0700

    f

---
+++ make/create.mk
@@ -0,0 +1,7 @@
+tf_create_new: # Plan and apply Terraform resources, require: TF_ROOT, environment
+	cd $(TF_ROOT)
+	@echo "Creating $(environment) resources..."
+	terraform plan -out=tfplan.out -var-file="$(TF_VARS_CONFIG)" -lock=false
+	@echo "Appling ..."
+	terraform apply -input=false -lock=false
+	rm -rf .terraform/terraform.tfstate>/dev/null 2>&1
