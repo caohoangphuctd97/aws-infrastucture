fd214c619d6ba8eff77bbc214c0ce4946e5be735
hoangphuc97 <phuchc@d-soft.com.vn>
2021-07-03 22:27:14 +0700

    save state in s3

--- make/create.mk
+++ make/create.mk
@@ -3,5 +3,5 @@ tf_create_new: # Plan and apply Terraform resources, require: TF_ROOT, environme
 	@echo "Creating $(environment) resources..."
 	terraform plan -out=tfplan.out -var-file="$(TF_VARS_CONFIG)" -lock=false
 	@echo "Appling ..."
-	terraform apply -input=false tfplan.out -lock=false
+	terraform apply -input=false tfplan.out
 	rm -rf .terraform/terraform.tfstate>/dev/null 2>&1
